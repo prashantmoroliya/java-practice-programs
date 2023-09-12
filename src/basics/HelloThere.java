885561a0896a4c0bc2edc133d5cc7f104edfb333
Darth Vader <25551044+prashantmoroliya@users.noreply.github.com>
2023-09-12 22:21:30 +0530

    Hello There! program

---
+++ src/basics/HelloThere.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class HelloThere {
+    public static void main(String[] args) {
+        // statement to print the Hello there by using System class and its method out;
+        // print...;
+        System.out.print("Hello "); // it will print Hello on first line, but it will not move cursor on new line.
+        // println;
+        System.out.println("There!");   // it will print There! and cursor will move to next line;
+    }
+}
