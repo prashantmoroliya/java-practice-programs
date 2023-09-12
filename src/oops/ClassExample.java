02acadeae483c598a426b77e9e68b8ea98315329
Darth Vader <25551044+prashantmoroliya@users.noreply.github.com>
2023-09-13 01:49:18 +0530

    class example program

---
+++ src/oops/ClassExample.java
@@ -0,0 +1,27 @@
+package oops;
+// This is a simple example of a class;
+// This program shows the structure of a class;
+
+// class name;
+public class ClassExample {
+    double radius=2.3;  // double variables;
+    String color="purple";   // String variable;
+
+    //methods;
+    double getRadius() {
+        // method body;
+        return radius;  // return statement;
+    }
+
+    String getColor() {
+        // method body;
+        return color;   // return statement;
+    }
+}
+
+/*
+
+
+        { This program has no output};
+
+ */
